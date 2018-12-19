3bb421e5f4bf8a6437684dd12f05cb35299f0e04
bazzie <bas@wildemann.nl>
2018-12-19 14:44:17 +0100

    error handling

--- cit/fetchGitTags.go
+++ cit/fetchGitTags.go
@@ -1,12 +1,14 @@
 package cit
 
 import (
+	"fmt"
 	"os/exec"
 )
 
 
 func FetchGetGitTags(d string) string {
 
+	fmt.Println("fetching tags")
 	cmd := exec.Command("git", "fetch", "--tags")
 	cmd.Dir = d
 	out, _ := cmd.CombinedOutput()
